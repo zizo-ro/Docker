3f19051534a94959c0e1709cdb888d2a7685633f
Fredy-SSA <alin@zizo.ro>
2021-11-25 21:35:17 +0200

    Docker

---
+++ _en/M-02/demo/11_Rest_Api/app.py
@@ -0,0 +1,18 @@
+# importing module
+from pandas import *
+import random
+import time
+
+
+# reading CSV file
+data = read_csv("jockes.csv")
+
+# converting column data to list
+ID= data['ID'].tolist()
+joke = data['Joke'].tolist()
+
+
+while True:
+    time.sleep(5)
+    r=random.randint(0,ID[-1])
+    print( "ID :"+ str(ID[r])+" Joke :"+joke[r] )
--- credits.md
+++ credits.md
@@ -12,3 +12,5 @@ Quiz :
 
 - M-02
 https://www.digitalocean.com/community/tutorials/how-to-build-and-deploy-a-flask-application-using-docker-on-ubuntu-18-04
+https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker
+https://github.com/amoudgl/short-jokes-dataset/edit/master/data/reddit-cleanjokes.csv
